b20b7fdb754d4b3da38ce0a1793fcba1011185e3
Masiel Bautista <masiel5@users.noreply.github.com>
2020-10-17 16:39:46 -0400

    started server file, missing mongodb

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+// Express
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Mongodb
+
+// Routes
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+// Server listening
+app.listen(PORT, () => {
+    console.log('App running on port ${PORT}!');
+});
